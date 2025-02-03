6573620ea5a22eba865c231c1bd2120be660a97a
Christophe Schulz <christopheschulz@orange.fr>
2025-02-03 15:35:10 +0100

    now

--- exo.py
+++ exo.py
@@ -2,11 +2,20 @@ import sys
 
 argument = sys.argv
 
-if len(argument) >2 or len(argument) == 1 or not argument[1].isdigit():
+
+
+
+
+if len(argument) >2 or len(argument) == 1  :
+    print("Tu ne me la mettras pas à l'envers")
+    exit()
+try:
+    int_arg = int(argument[1])
+except ValueError:
     print("Tu ne me la mettras pas à l'envers")
     exit()
 
-if int(argument[1]) % 2 == 0:
+if abs(int(argument[1])) % 2 == 0:
     print("pair")
 else:
     print("impair")
